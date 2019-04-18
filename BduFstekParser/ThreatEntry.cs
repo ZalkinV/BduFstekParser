eb4ff8a6160e5c2996c9f214c1fbbadd58c4f4b1
Zalkin <Zalkin.viktor@mail.ru>
2019-04-18 19:39:10 +0300

    TE. Add properties

--- BduFstekParser/ThreatEntry.cs
+++ BduFstekParser/ThreatEntry.cs
@@ -8,5 +8,13 @@ namespace BduFstekParser
 {
 	class ThreatEntry
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Intruder { get; set; }
+		public string Target { get; set; }
+		public bool IsConfident { get; set; }
+		public bool IsIntegrity { get; set; }
+		public bool IsAvailability { get; set; }
 	}
 }
